72a9bdc31823ff13e82da07270c6c9b3725680f0
alesshosry <alesshosry@hotmail.com>
2026-01-16 16:01:24 +0100

    redesign projects section

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,12 +8,40 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ProjectsComponent implements OnInit {
   companies: any[] = [];
+  expandedProjects: Set<any> = new Set();
+  expandedCompanies: Set<any> = new Set();
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.http.get('/assets/projects.json').subscribe((data: any) => {
       this.companies = data;
+      // Expand all companies by default
+      this.companies.forEach(company => this.expandedCompanies.add(company));
     });
+  }
+
+  toggleExpanded(project: any): void {
+    if (this.expandedProjects.has(project)) {
+      this.expandedProjects.delete(project);
+    } else {
+      this.expandedProjects.add(project);
+    }
+  }
+
+  isExpanded(project: any): boolean {
+    return this.expandedProjects.has(project);
+  }
+
+  toggleCompanyExpanded(company: any): void {
+    if (this.expandedCompanies.has(company)) {
+      this.expandedCompanies.delete(company);
+    } else {
+      this.expandedCompanies.add(company);
+    }
+  }
+
+  isCompanyExpanded(company: any): boolean {
+    return this.expandedCompanies.has(company);
   }
 }
